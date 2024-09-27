4763dac77725cc07405b248686582a22f7256e3c
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2024-09-27 20:06:54 +0200

    fix double shifting bug

--- nimJsTimezone.nimble
+++ nimJsTimezone.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.2.0"
+version       = "0.3.0"
 author        = "David Krause (enthus1ast)"
 description   = "Convert timezones in js; for example for html datetime-local"
 license       = "MIT"
--- src/nimJsTimezone.nim
+++ src/nimJsTimezone.nim
@@ -29,9 +29,7 @@ function convertTimeZone(
   return result
 }
 
-function dateToString(date, timeZone) {
-  date = convertTimeZone(date, 'UTC', timeZone)
-
+function dateToString(date) {
   const year = date.getUTCFullYear().toString().padStart(4, '0')
   const month = (date.getUTCMonth() + 1).toString().padStart(2, '0')
   const day = date.getUTCDate().toString().padStart(2, '0')
