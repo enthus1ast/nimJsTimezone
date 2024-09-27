9a1dfa2606cd71d4b413c725d29f931207975e57
David Krause (enthus1ast) <krause@biochem2.uni-frankfurt.de>
2024-09-27 21:41:39 +0200

    fix functionallity ;)

--- src/nimJsTimezone.nim
+++ src/nimJsTimezone.nim
@@ -30,12 +30,12 @@ function convertTimeZone(
 }
 
 function dateToString(date) {
-  const year = date.getUTCFullYear().toString().padStart(4, '0')
-  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0')
-  const day = date.getUTCDate().toString().padStart(2, '0')
-  const hours = date.getUTCHours().toString().padStart(2, '0')
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0')
-  const seconds = date.getUTCSeconds().toString().padStart(2, '0')
+  const year = date.getFullYear().toString().padStart(4, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
 
   return ``${year}-${month}-${day}T${hours}:${minutes}:${seconds}``
 }
